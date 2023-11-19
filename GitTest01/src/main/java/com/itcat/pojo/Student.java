c52a0e1a2bc2b4c926ebfdebd7117d6465637c4f
KariCat <1735016784@qq.com>
2023-11-19 22:49:31 +0800

    添加成员JavaBean

--- GitTest01/src/main/java/com/itcat/pojo/Student.java
+++ GitTest01/src/main/java/com/itcat/pojo/Student.java
@@ -5,4 +5,82 @@ public class Student {
     private Integer age;
     private double height;
     private String hobby;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age, double height, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.hobby = hobby;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * 设置
+     * @param height
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * 获取
+     * @return hobby
+     */
+    public String getHobby() {
+        return hobby;
+    }
+
+    /**
+     * 设置
+     * @param hobby
+     */
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String toString() {
+        return "Student{name = " + name + ", age = " + age + ", height = " + height + ", hobby = " + hobby + "}";
+    }
 }
